f05de7af10f80438f07ca0ab20ba9e954d9f5a05
anarottenbanana <lauraanaboompw@gmail.com>
2026-08-04 22:01:41 -0300

    adiciona conteúdo sobre binding e interpolação

--- fundamentos-alunos/src/app/pages/home/home.component.ts
+++ fundamentos-alunos/src/app/pages/home/home.component.ts
@@ -8,4 +8,13 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
 
+  titulo: string = 'Card Alunos'
+
+   nome: string = 'Ana'
+   turma: string = 'Adas dEV 2026'
+   status: string = 'Aprovado'
+   notaFinal: number = 9.5
+   idade: number = 17
+   imagem: string = 'https://i.redd.it/x9269yq508j71.jpg'
+
 }
